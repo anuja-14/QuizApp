c75c6e3e1c4b34460a9db813e87833b44eb48e10
anuja <anujaagrw@gmail.com>
2010-07-19 23:36:23 +0530

    authenticates the user by comparing the database password and the password in the request and forwards control to login.jsp

--- WEB-INF/classes/servlet/Loginctl.java
+++ WEB-INF/classes/servlet/Loginctl.java
@@ -1,35 +1,71 @@
 package servlet;
 import java.io.IOException;
-import QuizApp.*;
+
+
+import login.*;
 import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 
 public class Loginctl extends HttpServlet
 {
 
-	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+	public void doGet(HttpServletRequest request,HttpServletResponse response)
+	{	try
 	{
-		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String first_name = request.getParameter("first_name");
+		response.setContentType("text/html");
+		try
+		{
+		String password = request.getParameter("password");
+		String username = request.getParameter("username");
+		UserDTO us = new UserDTO();
+		UserService usv = new UserService();
+		us=usv.getUserInfo(username);
+		if(us.getPassword().equals(password))
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
+			rd.forward(request,response);
+		}
+		else
+		{	
+			String error="INVALID PASSWORD";
+			request.setAttribute( "err" ,error);
+			RequestDispatcher rd = request.getRequestDispatcher("/homepage.jsp");
+			rd.forward(request,response);
+		}
         out.println("<HTML>");
-        out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+        out.println("  <HEAD></HEAD>");
         out.println("  <BODY>");
-        out.print("  Hello World ");
+        out.print("");
         out.println("  </BODY>");
         out.println("</HTML>");
         out.flush();
         out.close();
+        
+		}
+		catch(Exception e)
+		{
+			String error="INVALID USERNAME";
+			request.setAttribute( "err" ,error);
+			RequestDispatcher rd = request.getRequestDispatcher("/homepage.jsp");
+			rd.forward(request,response);
+			
+		}
 		
+	}catch(Exception E)
+	{
+		
+	}
 	}
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
-	    throws ServletException, IOException {
-	doPost(request, response);
+    throws ServletException, IOException {
+doPost(request, response);
 }
 	
 	
